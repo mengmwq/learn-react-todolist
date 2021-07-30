8406bb57f47fd18cb3571c8e2bc5ad655a3d094b
mjk <535959887@qq.com>
2021-07-30 23:46:05 +0800

    路由小节的实现

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,10 @@
+
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div> <h3>我是Home页面</h3> </div>
+    );
+  }
+}
--- src_todolist/index.js
+++ src_todolist/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+{/* <React.StrictMode>检查app不合理的部分 */}
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter><App /></BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
